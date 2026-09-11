f35592555c771d92d1c67d833ef52ffda8eeee5b
Платон Петров <pplaton6@gmail.com>
2026-09-11 17:01:57 +0300

    Add hello.py: Hello appsec world (quick and dirty)

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# works with python2 and python3, cli, ipython -- whatever is around
+import sys,os
+MSG="Hello appsec world"
+def PRINT_IT(x):
+    if x==None: x=MSG
+    print(x); sys.stdout.flush(); return True
+if __name__=="__main__":
+    a=MSG
+    b=a
+    PRINT_IT(b)
+    os.system("true")
